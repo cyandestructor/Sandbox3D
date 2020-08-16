d669226c55ee7faed1486777730065e7e97bf191
Bryan Duarte <dellinspiron15BEDL@outlook.com>
2020-08-16 11:44:42 -0500

    02 Solved C4312 when casting unsigned int to const void*

--- Jass/src/Platform/OpenGL/OpenGLVertexArray.cpp
+++ Jass/src/Platform/OpenGL/OpenGLVertexArray.cpp
@@ -66,7 +66,7 @@ namespace Jass {
 				ToGLenum(element.Type),
 				element.Normalized ? GL_TRUE : GL_FALSE,
 				vertexBuffer->GetLayout().GetStride(),
-				(const void*)element.Offset);
+				(const void*)(uint64_t)element.Offset);
 			index++;
 		}
 
