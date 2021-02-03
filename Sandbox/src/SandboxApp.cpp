8ec4ea94e967d700e2445f6ad1825dda524a6954
Bryan Duarte <dellinspiron15BEDL@outlook.com>
2021-02-02 22:59:06 -0600

    01 Informative introduction to examples added

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,25 +1,43 @@
 #include <Jass.h>
 #include <Jass/Core/EntryPoint.h>
 
-//#include "Sandbox3D/Sandbox3D.h"
-//#include "Examples/ModelLoading/ModelLoading.h"
+#include "Examples/ModelLoading/ModelLoading.h"
 //#include "Examples/Transformations/Transformations.h"
 //#include "Examples/TerrainLoading/TerrainLoading.h"
 //#include "Examples/SkyboxLoading/SkyboxLoading.h"
 //#include "Examples/BillboardLoading/BillboardLoading.h"
-#include "Examples/WaterLoading/WaterLoading.h"
+//#include "Examples/WaterLoading/WaterLoading.h"
+//#include "Sandbox3D/Sandbox3D.h"
 
+/*
+	This is the main class of your application. It is divided into layers.
+	Your program probably will only need one layer, but you can experiment with applications
+	with more than one layer.
+
+	The following code can load some examples. Please check each in the order they are.
+	To load an example, uncomment its corresponding #include and its corresponding PushLayer.
+	ONLY PUSH ONE LAYER AT A TIME FOR EXAMPLES! SO IF YOU UNCOMMENT ONE, COMMENT THE LAST ONE ACTIVE.
+
+	Check the examples to know how to implement you own layer. Also, the examples give you
+	information about some features already include with the program (Terrain, models, water...)
+
+	All the examples have informative comments, but there are some things in some examples that is
+	explained in other ones, so please read and try them in order.
+
+	Thank you, enjoy and good luck!
+	- Bryan Duarte
+*/
 class Sandbox : public Jass::Application {
 
 public:
 	Sandbox() {
-		//PushLayer(new Sandbox3D);
-		//PushLayer(new ModelLoading);
+		PushLayer(new ModelLoading);
 		//PushLayer(new Transformations);
 		//PushLayer(new TerrainLoading);
 		//PushLayer(new SkyboxLoading);
 		//PushLayer(new BillboardLoading);
-		PushLayer(new WaterLoading);
+		//PushLayer(new WaterLoading);
+		//PushLayer(new Sandbox3D);
 	}
 
 	~Sandbox() {
